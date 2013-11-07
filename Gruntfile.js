8ee7fa772c94cc20e51872a8e159dbb66cad98bc
Izzimach <gary@interstellarechidna.com>
2013-11-07 14:48:59 -0700

    Initial files.

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        uglify: {
+          options: {
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+          },
+          build: {
+            src: 'src/*.js',
+            dest: 'build/scripts/<%= pkg.name %>.min.js'
+          }
+        },
+
+        browserify: {
+          basic: {
+            src: ['src/greyplains.js'],
+            dest: 'build/scripts/<%= pkg.name %>.min.js'
+          }
+        },
+
+
+        watch: {
+          files: ['src/*.js'],
+          tasks: ['browserify']
+        }
+
+      });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['browserify']);
+};
