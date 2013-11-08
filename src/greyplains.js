12c5d435066f52fc450c21ccb3419f7bb4829a4c
Izzimach <gary@interstellarechidna.com>
2013-11-07 18:13:39 -0700

    Fixed up some module references.

--- src/greyplains.js
+++ src/greyplains.js
@@ -1,6 +1,9 @@
-var Boot = require('./Boot');
 
-window.StartGame = function ()
+
+var Boot = require('./Boot');
+var Game = require('./Game');
+
+StartGame = function ()
 {
     //  Create your Phaser game and inject it into the gameContainer div.
     //  We did it in a window.onload event, but you can do it anywhere (requireJS load, anonymous function, jQuery dom ready, etc - whatever floats your boat)
@@ -8,13 +11,13 @@ window.StartGame = function ()
 
     //  Add the States your game has.
     //  You don't have to do this in the html, it could be done in your Boot state too, but for simplicity I'll keep it here.
-    //game.state.add('Boot', BasicGame.Boot);
+    game.state.add('Boot', Boot);
+    game.state.add('Game', Game);
     //game.state.add('Preloader', BasicGame.Preloader);
     //game.state.add('MainMenu', BasicGame.MainMenu);
-    //game.state.add('Game', BasicGame.Game);
 
     //  Now start the Boot state.
-    //game.state.start('Boot');
+    game.state.start('Boot');
 
 
 };
